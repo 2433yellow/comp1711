c228b749fbd7a17f213228d6b087926857198987
Leyla Ali <146722024+2433yellow@users.noreply.github.com>
2023-10-09 11:55:10 +0000

    bootcamp session 1

---
+++ IntegerSummation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+float D ;
+float B ;
+float A ;
+printf("Enter integer 1: ");
+scanf("%f",&D);
+printf("\n Enter integer 2: ");
+scanf ("%f",&B);
+A=D+B;
+printf("\n the Addition of both is: %f\n ",A);
+return 0;
+}
